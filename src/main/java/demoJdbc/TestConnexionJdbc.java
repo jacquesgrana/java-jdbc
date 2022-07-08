048de1e142299a53ece038543e8b63931f8bfe0e
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-07-08 15:06:43 +0200

    TP2 jdbc - Exo 1 améliorations

--- src/main/java/demoJdbc/TestConnexionJdbc.java
+++ src/main/java/demoJdbc/TestConnexionJdbc.java
@@ -6,15 +6,26 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
+	
+	private static final String DB_URL;
+	private static final String DB_USER;
+	private static final String DB_PW;
+	
+	static {
+		ResourceBundle bundle = ResourceBundle.getBundle("database");
+		DB_URL = bundle.getString("database.url");
+		DB_USER = bundle.getString("database.username");
+		DB_PW = bundle.getString("database.password");
+	}
 
 	public static void main(String[] args) {
-		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
+		
 		//String driverName = monFichierConf.getString("database.driver");
-		String dbUrl = monFichierConf.getString("database.url");
-		String dbUser = monFichierConf.getString("database.username");
-		String dbPw = monFichierConf.getString("database.password");
+		//String dbUrl = monFichierConf.getString("database.url");
+		//String dbUser = monFichierConf.getString("database.username");
+		//String dbPw = monFichierConf.getString("database.password");
 		try {
-			Connection connection1 = DriverManager.getConnection(dbUrl, dbUser, dbPw);
+			Connection connection1 = DriverManager.getConnection(DB_URL, DB_USER, DB_PW);
 			System.out.println("Connection ok !\n");
 			System.out.println("connection : " + connection1 + "\n");
 			connection1.close();
